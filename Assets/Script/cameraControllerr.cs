dd75825bfe6f4fc5e2c1bcff87e8cbefb27041d7
1030551159 <1030551159@qq.com>
2019-07-18 19:50:13 +0800

    map+

--- Assets/Script/cameraControllerr.cs
+++ Assets/Script/cameraControllerr.cs
@@ -21,39 +21,33 @@ public class cameraControllerr : MonoBehaviour
         if (Input.GetAxis("Mouse ScrollWheel") < 0)
         {
             Debug.Log("hua");
-           if (Camera.main.transform.position.z - center.transform.position.z > -120)
-           {
-                cameraMove(0, 0, -2);
-           }
+            cameraMove(0, -2, 0);
         }
         if (Input.GetAxis("Mouse ScrollWheel") > 0)
         {
-            if (Camera.main.transform.position.z - center.transform.position.z < -70)
-            {
-                cameraMove(0, 0, 2);
-            }
+            cameraMove(0, 2, 0);
         }
     }
     public void moveLeft()
     {
-        StartCoroutine(ieMove(-0.2f,0,0,1));
+        StartCoroutine(ieMove(-2f,0,0,1));
     }
 
     public void moveRight()
     {
-        StartCoroutine(ieMove(0.2f, 0,0,2));
+        StartCoroutine(ieMove(2f, 0,0,2));
     }
 
     public void moveUp()
     {
         
-            StartCoroutine(ieMove(0, 0.2f,0,3));
+            StartCoroutine(ieMove(0, 0, 2f, 3));
     }
 
     public void moveDown()
     {
         
-            StartCoroutine(ieMove(0, -0.2f,0,4));
+            StartCoroutine(ieMove(0, 0, -2f, 4));
     }
 
     public void stopMove()
@@ -65,17 +59,8 @@ public class cameraControllerr : MonoBehaviour
     {
         while(true)
         {
-            if (left.transform.localPosition.x - center.transform.localPosition.x < -400&&dir==1)
-                break;
-            if (right.transform.localPosition.x - center.transform.localPosition.x > 400&&dir==2)
-                break;
-            if (up.transform.localPosition.y - center.transform.localPosition.y > 250&&dir==3)
-                break;
-            if (down.transform.localPosition.y - center.transform.localPosition.y < -250&&dir==4)
-                break;
             cameraMove(x, y, z);
-
-            yield return 0.02f;
+            yield return new WaitForSeconds(0.02f);
         }
        
     }
@@ -87,29 +72,6 @@ public class cameraControllerr : MonoBehaviour
         cameraPos.z = cameraPos.z + z;
         Camera.main.transform.position = cameraPos;
 
-        Vector3 lpos = left.transform.position;
-        lpos.x = left.transform.position.x + x;
-        lpos.y = left.transform.position.y + y;
-        lpos.z = left.transform.position.z + z;
-        left.transform.position = lpos;
-
-        Vector3 rpos = right.transform.position;
-        rpos.x = right.transform.position.x + x;
-        rpos.y = right.transform.position.y + y;
-        rpos.z = right.transform.position.z + z;
-        right.transform.position = rpos;
-
-        Vector3 upos = up.transform.position;
-        upos.x = up.transform.position.x + x;
-        upos.y = up.transform.position.y + y;
-        upos.z = up.transform.position.z + z;
-        up.transform.position = upos;
-
-        Vector3 dpos = down.transform.position;
-        dpos.x = down.transform.position.x + x;
-        dpos.y = down.transform.position.y + y;
-        dpos.z = down.transform.position.z + z;
-        down.transform.position = dpos;
     }
 
 }
